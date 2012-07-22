9902ff881d747608b0796bdf0349dfb0f724184b
Dave Cole <dave@developmentseed.org>
2012-07-22 02:04:26 -0400

    Improved script; add web hook

---
+++ hook.rb
@@ -0,0 +1,12 @@
+# hook.rb
+require 'sinatra'
+require 'json'
+
+post "/api" do
+  request.body.rewind  # in case someone already read it
+  data = JSON.parse request.body.read
+  sha = data['after']
+  repos = data['repository']['name']
+  user = data['repository']['owner']['name']
+  `./run.sh #{user} #{repos} #{sha} &> out.txt`
+end
