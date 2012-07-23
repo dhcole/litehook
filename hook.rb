e94726b12a61fcd107c908076c01d8fae2f980b9
Dave Cole <dave@developmentseed.org>
2012-07-23 17:40:45 -0400

    webhook

--- hook.rb
+++ hook.rb
@@ -4,9 +4,13 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 
+get "*" do
+    ""
+end
+
 post "/api" do
   request.body.rewind  # in case someone already read it
-  data = JSON.parse request.body.read
+  data = JSON.parse(params[:payload])
   sha = data['after']
   repos = data['repository']['name']
   user = data['repository']['owner']['name']
